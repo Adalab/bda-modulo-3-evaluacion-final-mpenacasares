12146cc376aa6642a8c9ee9150cce30a20cea695
Macarena Peña <m.penacasares@gmail.com>
2024-11-27 05:42:59 +0100

    Fase 3 bonus finalizada

--- src/soporte.py
+++ src/soporte.py
@@ -18,8 +18,11 @@ import scipy.stats as stats
 import scipy.stats as stats
 from scipy.stats import shapiro, poisson, chisquare, expon, kstest
 
-# Fase 1: Exploración y Limpieza
+# --------------------------------------🚨NOTA🚨--------------------------------------------
+# Si hago cambios aqui tengo que importarme de nuevo soporte donde vaya a usarlo y a veces incluso restaurar kernel
 
+
+# Fase 1: Exploracion_Limpieza
 def minus_guion_bajo(df):
     """
     Función para reemplazar los espacios en las columnas por guiones bajos
@@ -34,3 +37,62 @@ def minus_guion_bajo(df):
     # Convertir nombres de columnas a minúsculas y reemplazar espacios por guiones bajos
     df.columns = [col.lower().replace(" ", "_") for col in df.columns]
     return df
+
+
+# Fase 3: Bonus
+def normalidad(dataframe, columna):
+    """
+    Evalúa la normalidad de una columna de datos de un DataFrame utilizando la prueba de Shapiro-Wilk.
+
+    Parámetros:
+        dataframe (DataFrame): El DataFrame que contiene los datos.
+        columna (str): El nombre de la columna en el DataFrame que se va a evaluar para la normalidad.
+
+    Returns:
+        None: Imprime un mensaje indicando si los datos siguen o no una distribución normal.
+    """
+
+    statistic, p_value = stats.shapiro(dataframe[columna])
+    if p_value > 0.05:
+        print(f"Para la columna {columna} los datos siguen una distribución normal.")
+    else:
+        print(f"Para la columna {columna} los datos no siguen una distribución normal.")
+
+
+def test_man_whitney(
+    dataframe, columnas_metricas, grupo_control, grupo_test, columna_grupos
+):
+    """
+    Realiza la prueba de Mann-Whitney U para comparar las medianas de las métricas entre dos grupos en un DataFrame dado.
+
+    Parámetros:
+    - dataframe (DataFrame): El DataFrame que contiene los datos.
+    - columnas_metricas (list): Una lista de nombres de columnas que representan las métricas a comparar entre los grupos.
+    - grupo_control (str): El nombre del grupo de control en la columna especificada por columna_grupos.
+    - grupo_test (str): El nombre del grupo de test en la columna especificada por columna_grupos.
+    - columna_grupos (str): El nombre de la columna que contiene la información de los grupos.
+
+    Returns
+    No devuelve nada directamente, pero imprime en la consola si las medianas son diferentes o iguales para cada métrica.
+    Se utiliza la prueba de Mann-Whitney U para evaluar si hay diferencias significativas entre los grupos.
+    """
+    # filtramos el DataFrame para quedarnos solo con los datos de control
+    control = dataframe[dataframe[columna_grupos] == grupo_control]
+
+    # filtramos el DataFrame para quedarnos solo con los datos de control
+    test = dataframe[dataframe[columna_grupos] == grupo_test]
+
+    # iteramos por las columnas de las metricas para ver si para cada una de ellas hay diferencias entre los grupos
+    for metrica in columnas_metricas:
+
+        # filtrams el conjunto de datos para quedarnos solo con la columna de la metrica que nos interesa
+        metrica_control = control[metrica]
+        metrica_test = test[metrica]
+
+        # aplicamos el estadístico
+        u_statistic, p_value = stats.mannwhitneyu(metrica_control, metrica_test)
+
+        if p_value < 0.05:
+            print(f"Para la métrica {metrica}, las medianas son diferentes.")
+        else:
+            print(f"Para la métrica {metrica}, las medianas son iguales.")
